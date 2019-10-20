daa4994460034714b70f40f78cc54877f144d116
Ygor.Macedo <ygormacedo44@gmail.com>
2019-10-20 13:36:09 -0300

    Spinner <DROPDOWN>

--- app/src/main/java/br/com/zupandroid/dadosdousuario/MainActivity.java
+++ app/src/main/java/br/com/zupandroid/dadosdousuario/MainActivity.java
@@ -1,7 +1,13 @@
 package br.com.zupandroid.dadosdousuario;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSipinner();
+    }
+
+    private void setSipinner() {
+
+        Spinner estadosDynamic = findViewById(R.id.estadosId);
+
+        // Create an ArrayAdapter using the string array and a default spinner
+        ArrayAdapter<CharSequence> staticAdapter = ArrayAdapter
+                .createFromResource(this, R.array.estados_array,
+                        android.R.layout.simple_spinner_item);
+
+        staticAdapter
+                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        estadosDynamic.setAdapter(staticAdapter);
+        estadosDynamic.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                Log.v("item", (String) parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
+        });
+
+        Spinner schoolDinamic = findViewById(R.id.escolaId);
+
+        // Create an ArrayAdapter using the string array and a default spinner
+        staticAdapter = ArrayAdapter
+                .createFromResource(this, R.array.escolaridade_array,
+                        android.R.layout.simple_spinner_item);
+
+        staticAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        schoolDinamic.setAdapter(staticAdapter);
+
+        schoolDinamic.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                Log.v("item", (String) parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
+        });
     }
 }
